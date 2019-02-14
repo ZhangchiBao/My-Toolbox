2ab6ebf4f89059f6f8e2833432ec457e0278a0b2
Zhangchi Bao <baozhangchi@live.com>
2019-02-14 21:55:42 +0800

    修改扒图插件

---
+++ BZ.WindowsService/PluginTests/PicReptilePluginTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MeizituReptile;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PluginTests
+{
+    [TestClass]
+    public class PicReptilePluginTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            new PicReptilePlugin().Test();
+        }
+    }
+}
--- BZ.WindowsService/Plugins/MeizituReptile/PicReptilePlugin.cs
+++ BZ.WindowsService/Plugins/MeizituReptile/PicReptilePlugin.cs
@@ -1,26 +1,31 @@
 ﻿using BZ.WindowsService.Common;
 using HtmlAgilityPack;
-using System.Collections.Generic;
-using System.Net.Http;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Net.Http;
 
 namespace MeizituReptile
 {
     public class PicReptilePlugin : IPlugin
     {
-        public PluginData PluginData => throw new System.NotImplementedException();
+        public PluginData PluginData => new PluginData
+        {
+            Author = "",
+            Name = "妹子图爬虫",
+            Version = "v1.0"
+        };
 
         private readonly string baseURL = @"http://www.meizitu.com/a/more_{0}.html";
 
         public void Start()
         {
             List<string> links = new List<string>();
-            int pageIndex = 0;
+            int pageIndex = 1;
             HtmlWeb web = new HtmlWeb();
             do
             {
-                var doc = web.Load(string.Format(baseURL, pageIndex));
+                var doc = web.Load(string.Format(baseURL, pageIndex++));
                 var linkNodes = doc.DocumentNode.SelectNodes(@"//ul[@class='wp-list clearfix']/li[@class='wp-item']/div[@class='con']/div[@class='pic']/a");
                 foreach (var linkNode in linkNodes)
                 {
@@ -37,10 +42,10 @@ namespace MeizituReptile
 
         private async void DownloadImage(string url, string title)
         {
-            using(var client=new HttpClient())
+            using (var client = new HttpClient())
             {
                 var uri = new Uri(url);
-                var buffer = await client.GetByteArrayAsync(uri);
+                var buffer = await client.GetByteArrayAsync(url);
                 File.WriteAllBytes(uri.OriginalString, buffer);
             }
         }
